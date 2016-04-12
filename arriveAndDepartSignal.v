c26fbe2989e3c4bd18538cd6f86079f04a4057ca
Quan Nguyen <quanuw@gmail.com>
2016-04-11 18:23:16 -0700

    need to check out the assign logic

--- arriveAndDepartSignal.v
+++ arriveAndDepartSignal.v
@@ -1,11 +1,17 @@
+// module for arrival, departure, inner port, and outer port
 module arriveAndDepartSignal(arriveSignal, leaveSignal, arriveSwitch,
-    leaveSwitch, bathIn, clk, rst);
+    leaveSwitch, timer, rst);
     output arriveSignal;
     output leaveSignal;
     input arriveSwitch;
     input leaveSwitch;
-    input bathIn;
-    input clk, rst;
+    input pressureUp;
+    input pressureDown;
+    input timer, rst;
 
-assign arriveSignal = arriveSwitch && bathIn;
+
+    assign arriveSignal = arriveSwitch && pressureUp;
+    assign leaveSignal = leaveSwitch && pressureDown;
+    assign outPort = pressureUp;
+    assign inPort = pressureDown;
 endmodule
