c7b280fb76e9eaae5eb2892efa0ed0d6abb70719
bradleymarquez <bradleymarquez@hotmail.com>
2016-04-11 18:09:48 -0700

    Counter

---
+++ counter.v
@@ -0,0 +1,20 @@
+// Counts up to 1023 seconds
+// Use clock with frequency 1 Hz
+// counterSeconds == 300 for 5 minutes
+// counterSeconds == 420 for 7 minutes
+// counterSeconds == 480 for 8 minutes
+module counter (clk, reset, counterSeconds, signal);
+	input clk, reset;
+	input [9:0] counterSeconds;
+	output wire signal;
+	reg count;
+	always @(posedge clk) begin
+		if (reset) begin
+			count <= 1'b0;
+		end else begin
+			count <= count + 10'b1;
+		end
+	end
+	assign signal = count == (counterSeconds - 1);
+endmodule
+				
