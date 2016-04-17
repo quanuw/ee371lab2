a0f667168e0a7c9ba3fd300e1cbe3dca88449ce9
Quan Nguyen <quanuw@gmail.com>
2016-04-16 21:01:33 -0700

    arrive and depart

--- arriveAndDepartSignal.v
+++ arriveAndDepartSignal.v
@@ -1,17 +1,63 @@
 // module for arrival, departure, inner port, and outer port
-module arriveAndDepartSignal(arriveSignal, leaveSignal, arriveSwitch,
-    leaveSwitch, timer, rst);
+
+`include "Metastability.v"
+`include "counter.v"
+
+module arriveAndDepartSignal(arriveSignal, departSignal, arriveSwitch,
+    departSwitch, clk, rst);
     output arriveSignal;
-    output leaveSignal;
+    output departSignal;
     input arriveSwitch;
-    input leaveSwitch;
+    input departSwitch;
     input pressureUp;
     input pressureDown;
-    input timer, rst;
+    input clk, rst;
+
+    reg departPS;
+    reg departNS;
+    reg arrivePS;
+    reg arriveNS;
+    parameter A = 0, B = 1;
+    Metastability arrive(.clk(), .rst(), .press(arriveSwitch), .metaFree(arriveSignal));
+    Metastability depart(.clk(), .rst(), .press(departSwitch), .metaFree(departSignal));
+    counter pressureUpCount(.clk(), .reset(), .counterSeconds(10'b111), .signal(pressureUp));
+    counter pressureDownCount(.clk(), .reset(), .counterSeconds(10'b111), .signal(pressureDown));
 
 
-    assign arriveSignal = arriveSwitch && pressureUp;
-    assign leaveSignal = leaveSwitch && pressureDown;
-    assign outPort = pressureUp;
-    assign inPort = pressureDown;
+    assign arriveSignal = arrivePS;
+    assign departSignal = departPS;
+
+    always @(*)
+        /*if (rst) begin
+            departNS = 0;
+            arriveNS = 0;
+        end*/
+        begin
+            if (arriveSwitch && pressureUp > 0 && pressureDown == 0) begin
+                arriveNS = 1;
+            end
+            else if (arrivePS && pressureUp > 0) begin
+                arriveNS = 1;
+            end
+            if (departSwitch && pressureDown > 0 && pressureUp == 0) begin
+                departNS = 1;
+            end
+            else if (departPS && pressureDown > 0) begin
+                departNS = 1;
+            end
+            else begin
+                arriveNS = 0;
+                departNS = 0;
+            end
+        end
+
+    always@(posedge clk)
+    	if(rst) begin
+    		arrivePS <= 0;
+            departPS <= 0;
+    	end
+    	else begin
+            arrivePS <= arriveNS;
+            departPS <= departNS;
+    	end
 endmodule
