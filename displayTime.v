94e83c78f420375c61de9bc58c19c7597f54a458
bradleymarquez <bradleymarquez@hotmail.com>
2016-04-22 15:08:42 -0700

    display

--- displayTime.v
+++ displayTime.v
@@ -2,7 +2,7 @@ module displayTime (Clock, Reset, countArrive, countFandP, countEvacuate, HEXArr
 	input Clock, Reset; 
 	input [9:0] countArrive, countFandP;
 	input [9:0] countEvacuate;
-	output wire [6:0] HEXArrive, HEXFandP, HEXEvacuate;
+	output reg [6:0] HEXArrive, HEXFandP, HEXEvacuate;
 	
 	// State encoding.
 	parameter [6:0] A = 7'b1000000, B = 7'b1111001, C = 7'b0100100, D = 7'b0110000, E = 7'b0011001, F = 7'b0010010, G = 7'b0000010, H = 7'b1111000, I = 7'b1111111;
@@ -12,40 +12,43 @@ module displayTime (Clock, Reset, countArrive, countFandP, countEvacuate, HEXArr
 		case(Reset)
 			1: begin
 				case (countArrive)
-					3'b0000000000: HEXArrive = A;
-					3'b0000000001: HEXArrive = B;
-					3'b0000000010: HEXArrive = C;
-					3'b0000000011: HEXArrive = D;
-					3'b0000000100: HEXArrive = E;
+					10'b0000000000: HEXArrive  = A;
+					10'b0000000001: HEXArrive  = B;
+					10'b0000000010: HEXArrive  = C;
+					10'b0000000011: HEXArrive  = D;
+					10'b0000000100: HEXArrive  = E;
+					default: HEXArrive  = 7'b1111111;
 				endcase
 				
-				case (countFandP)
-					3'b0000000000: HEXFandP = A;
-					3'b0000000001: HEXFandP = B;
-					3'b0000000010: HEXFandP = C;
-					3'b0000000011: HEXFandP = D;
-					3'b0000000100: HEXFandP = E;
-					3'b0000000101: HEXFandP = F;
-					3'b0000000110: HEXFandP = G;
-					3'b0000000111: HEXFandP = H;
+				case(countFandP)
+					10'b0000000000: HEXFandP  = A;
+					10'b0000000001: HEXFandP  = B;
+					10'b0000000010: HEXFandP  = C;
+					10'b0000000011: HEXFandP  = D;
+					10'b0000000100: HEXFandP  = E;
+					10'b0000000101: HEXFandP  = F;
+					10'b0000000110: HEXFandP  = G;
+					10'b0000000111: HEXFandP  = H;
+					default: HEXFandP  = 7'b1111111;
 				endcase
 				
 				case (countEvacuate)
-					3'b0000000000: HEXEvacuate = A;
-					3'b0000000001: HEXEvacuate = B;
-					3'b0000000010: HEXEvacuate = C;
-					3'b0000000011: HEXEvacuate = D;
-					3'b0000000100: HEXEvacuate = E;
-					3'b0000000101: HEXEvacuate = F;
-					3'b0000000110: HEXEvacuate = G;
-					3'b0000000111: HEXEvacuate = H;
-					4'b0000001000: HEXEvacuate = I;
+					10'b0000000000: HEXEvacuate  = A;
+					10'b0000000001: HEXEvacuate  = B;
+					10'b0000000010: HEXEvacuate  = C;
+					10'b0000000011: HEXEvacuate  = D;
+					10'b0000000100: HEXEvacuate  = E;
+					10'b0000000101: HEXEvacuate  = F;
+					10'b0000000110: HEXEvacuate  = G;
+					10'b0000000111: HEXEvacuate  = H;
+					10'b0000001000: HEXEvacuate  = I;
+					default: HEXEvacuate = 7'b1111111;
 				endcase
 			end
 			0: begin
-				HEXArrive = A;
-				HEXFandP = A;
-				HEXEvacuate = A;
+				HEXArrive  = A;
+				HEXFandP  = A;
+				HEXEvacuate  = A;
 			end 
 		endcase
 	end
