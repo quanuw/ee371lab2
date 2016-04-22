2d3c6978af9f9573d9b0d176905916ab6a779f29
Quan Nguyen <quanuw@gmail.com>
2016-04-21 19:56:20 -0700

    new logic

--- arriveAndDepartSignal.v
+++ arriveAndDepartSignal.v
@@ -19,25 +19,25 @@ module arriveAndDepartSignal(arriveSignal, departSignal, arriveSwitch,
     Metastability arrive(.clk(), .rst(), .press(arriveSwitch), .metaFree(arriveSignal)); // prevent metastability from the signals
     Metastability depart(.clk(), .rst(), .press(departSwitch), .metaFree(departSignal));
 
-    counter pressureUpCount(.clk(clk), .reset(rst), .counterSeconds(10'b111), .signal(pressureUp)); // interface with counters
-    counter pressureDownCount(.clk(clk), .reset(rst), .counterSeconds(10'b111), .signal(pressureDown));
-
+    //counter pressureUpCount(.clk(clk), .reset(rst), .counterSeconds(10'b111), .signal(pressureUp)); // interface with counters
+    //counter pressureDownCount(.clk(clk), .reset(rst), .counterSeconds(10'b111), .signal(pressureDown));
+    //FillandPressurize isPressurized(.Clock, .Reset, .begin_FandP, .InnerClosed, .OuterClosed, .Evacuated, .Pressurized, .FandP(pressureUp));
 
     assign arriveSignal = arrivePS; // assign the signals to present states
     assign departSignal = departPS;
 
     always @(*)
         begin
-            if (arriveSwitch && pressureUp == 0) begin // if switch is enabled and pressureUp counter is greater than 0 && pressureDown counter is 0
+            if (arriveSwitch && FPState == 0) begin // if switch is enabled and pressureUp counter is greater than 0 && pressureDown counter is 0
                 arriveNS = 1;
             end
-            else if (arrivePS) begin // if present state is on and pressureUp counter is greater than 0 next
+            else if (arrivePS && FPState == 0) begin // if present state is on and pressureUp counter is greater than 0 next
                 arriveNS = 1;
             end
-            if (departSwitch && pressureDown > 0 && pressureUp == 0) begin // if switch is enabled and pressureDown counter is greater than 0 && pressureUp counter is 0
+            if (departSwitch &&  EvState == 0) begin // if switch is enabled and pressureDown counter is greater than 0 && pressureUp counter is 0
                 departNS = 1;
             end
-            else if (departPS && pressureDown > 0) begin // if present state is on and pressureDown counter is greater than o
+            else if (departPS && EVState == 0) begin // if present state is on and pressureDown counter is greater than o
                 departNS = 1;
             end
             else begin // else turn off both signals
