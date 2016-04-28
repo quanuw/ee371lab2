8d89935afea8a5b73146358788c04f01e6c55600
bradleymarquez <bradleymarquez@hotmail.com>
2016-04-27 22:45:58 -0700

    updated counter stuff

--- counter_testbench.v
+++ counter_testbench.v
@@ -2,18 +2,17 @@
 `include "counter_tester.v"
 
 module counter_testbench();
-
-	wire Clock, Reset, beginCount;
-	wire [9:0] counterSeconds;
-	wire [9:0] signal;
+	wire Clock, Reset, beginCount, start, countInProcess;
+	wire [9:0] counterSeconds, count;
+	wire signal;
 	
 	// Calls the 1 bit counter tester module 
 	// Notice that the output Clock, Reset, Increase of the tester module are wires Clock, Reset, Increase in this module
-	counter_tester t(.Clock(Clock), .Reset(Reset), .beginCount(beginCount), .counterSeconds(counterSeconds));
+	counter_tester t(.Clock(Clock), .Reset(Reset), .start(start), .counterSeconds(counterSeconds));
 	
 	// Calls the 1 bit counter module as the device under test (dut)
 	// Notice that the Clock, Reset, and Increase are wired to the input of the 1 bit counter module  
-	counter dut(.clk(Clock), .reset(Reset), .beginCount(beginCount), .counterSeconds(counterSeconds), .signal(signal));
+	counter dut(.clk(Clock), .reset(Reset), .counterSeconds(counterSeconds), .start(start), .signal(signal), .countInProcess(countInProcess), .count(count));
 	
 	// Dumps the results of the simulation into a .vcd file for view in GTKWave
 	initial begin 
--- counter_tester.v
+++ counter_tester.v
@@ -1,5 +1,5 @@
-module counter_tester(Clock, Reset, beginCount, counterSeconds);
-	output reg Clock, Reset, Increase, beginCount;
+module counter_tester(Clock, Reset, start, counterSeconds);
+	output reg Clock, Reset, Increase, start;
 	output reg [9:0] counterSeconds;
 	
 	// Set up the clock.
@@ -19,8 +19,8 @@ module counter_tester(Clock, Reset, beginCount, counterSeconds);
 										@(posedge Clock);
 										@(posedge Clock);
 										@(posedge Clock);
-		beginCount <= 1;				@(posedge Clock);
-		beginCount <= 0;				@(posedge Clock);
+		start <= 1;				@(posedge Clock);
+		start <= 0;				@(posedge Clock);
 										@(posedge Clock);
 										@(posedge Clock);
 										@(posedge Clock);
@@ -42,117 +42,4 @@ module counter_tester(Clock, Reset, beginCount, counterSeconds);
 										@(posedge Clock);										
 
 	end
-	
-/*
-=======
-module counter_tester(Clock, Reset, Start);
-	output reg Clock, Reset, Start;
-	
-	// Set up the clock.
-	parameter CLOCK_PERIOD=2;
-	initial Clock=1;
-	always begin
-		#(CLOCK_PERIOD/2);
-		Clock = ~Clock;
-	end
-	
-	// Set up the inputs to the design. Each line is a clock cycle.
-	initial begin
-		Reset <= 1;					@(posedge Clock);			
-		Reset <= 0;					@(posedge Clock);
-		Reset <= 1;					@(posedge Clock);	
-		Start <= 0;					@(posedge Clock);
-		Start <= 1;					@(posedge Clock);
-		Start <= 0;					@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);	
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);	
-
-	end
->>>>>>> 65c9eb1007036024b958e6fe0086b78dfac62c23
-=======
-/*
-module counter_tester(Clock, Reset, Start);
-	output reg Clock, Reset, Start;
-	
-	// Set up the clock.
-	parameter CLOCK_PERIOD=2;
-	initial Clock=1;
-	always begin
-		#(CLOCK_PERIOD/2);
-		Clock = ~Clock;
-	end
-	
-	// Set up the inputs to the design. Each line is a clock cycle.
-	initial begin
-		Reset <= 1;					@(posedge Clock);			
-		Reset <= 0;					@(posedge Clock);
-		Reset <= 1;					@(posedge Clock);	
-		Start <= 0;					@(posedge Clock);
-		Start <= 1;					@(posedge Clock);
-		Start <= 0;					@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);	
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-		Start <= 0;					@(posedge Clock);
-		Start <= 1;					@(posedge Clock);
-		Start <= 0;					@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);	
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);	
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);
-										@(posedge Clock);	
-
-	end
-// >>>>>>> f0f5a8d88cdd457b58cf8f796f57b14ad7e54329
 endmodule
-*/
